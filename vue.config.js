edf3377e35113dd7b69f36e3527e7eb667a68c22
Linxi-ovo <2809837498@qq.com>
2025-10-20 15:20:12 +0800

    修复Inavlid host header的问题

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  devServer: {
+    host: '0.0.0.0',
+    allowedHosts: ["all"
+    ],
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3000',
+        changeOrigin: true,
+        secure: false,
+        logLevel: 'debug'
+      }
+    }
+  }
 })
